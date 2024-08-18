7c4423b1695cb0d07771edba1f5684c0e2074b7c
kiruhak <kiruhak1991@gmail.com>
2024-08-18 20:02:13 +0700

    Task 1

--- app.vue
+++ app.vue
@@ -1,57 +1,56 @@
 <template>
-    <div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-        <div>{{ hui }} {{ nazvaniye }}</div>
-
-        <p>{{ nazvaniye }}</p>
-        <p>{{ hui }}</p>
-    </div>
+    <body>
+        <div class="container">
+            <div class="pyramid left">
+                <div class="block small"></div>
+                <div class="block medium"></div>
+                <div class="block large"></div>
+            </div>
+            <div class="pyramid center">
+                <div class="block small"></div>
+                <div class="block medium"></div>
+                <div class="block large"></div>
+            </div>
+            <div class="pyramid right">
+                <div class="block small"></div>
+                <div class="block medium"></div>
+                <div class="block large"></div>
+            </div>
+        </div>
+    </body>
 </template>
 
-<script lang="ts" setup>
-const nazvaniye = ref('text');
-const hui = ref('200$');
-</script>
+<script lang="ts" setup></script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.container {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+}
+.pyramid {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.pyramid.left {
+    align-items: flex-start;
+}
+.pyramid.right {
+    align-items: flex-end;
+}
+.block {
+    height: 50px;
+    background-color: red;
+    margin: 2px 0;
+}
+.block.small {
+    width: 50px;
+}
+.block.medium {
+    width: 100px;
+}
+.block.large {
+    width: 150px;
+}
+</style>
